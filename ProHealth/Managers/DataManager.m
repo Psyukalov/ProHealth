a0b0e2525c23dbd95de05526bf54ec09cd2d18da
Vladimir Psyukalov <vladimirpsyukalov@mac-mini-administrator.local>
2015-11-24 17:38:33 +0300

    add files data manager

---
+++ ProHealth/Managers/DataManager.m
@@ -0,0 +1,13 @@
+//
+//  DataManager.m
+//  ProHealth
+//
+//  Created by Vladimir Psyukalov on 24.11.15.
+//  Copyright © 2015 Natalia Zubareva. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
+@end
