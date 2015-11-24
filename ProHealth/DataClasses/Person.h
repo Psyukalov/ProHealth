d48ac98c09a265c8f1890d77809f32fe74250959
panenko <konstantin.panenko@gmail.com>
2015-11-24 12:32:41 +0300

    add person class

---
+++ ProHealth/DataClasses/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  ProHealth
+//
+//  Created by Admin on 24.11.15.
+//  Copyright © 2015 Natalia Zubareva. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface Person : NSObject
+
+
+
+@end
---
+++ ProHealth/DataClasses/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  ProHealth
+//
+//  Created by Admin on 24.11.15.
+//  Copyright © 2015 Natalia Zubareva. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
