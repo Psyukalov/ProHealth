c8a755c7b2b19befc013efb2ed48fb9d652d0086
Vladimir Psyukalov <psyukalov@gmail.com>
2015-11-27 16:36:51 +0300

    finishing core data

--- ProHealth/ViewControllers/PersonalStatsViewController.m
+++ ProHealth/ViewControllers/PersonalStatsViewController.m
@@ -6,16 +6,19 @@
 //  Copyright © 2015 Natalia Zubareva. All rights reserved.
 //
 
+
 #import "PersonalStatsViewController.h"
 #import "SettingsViewController.h"
 #import "DataManager.h"
 #import "Eating+CoreDataProperties.h"
 #import "Eating.h"
 
+
 @interface PersonalStatsViewController ()
 
 @property (strong, nonatomic) Person *person;
 @property (strong, nonatomic) DataManager *context;
+@property (strong, nonatomic) NSCalendar *calendar;
 @property (strong, nonatomic) NSMutableArray *calories;
 @property (weak, nonatomic) IBOutlet UIView *contentView;
 @property (weak, nonatomic) IBOutlet UIView *histogramView;
@@ -23,10 +26,9 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imageCircleAvatar;
 @property (weak, nonatomic) IBOutlet UILabel *lblName;
 @property (weak, nonatomic) IBOutlet UILabel *lblStartDate;
-//tmp
-@property (weak, nonatomic) IBOutlet UITextField *txt;
 
 @end
+
 
 @implementation PersonalStatsViewController
 
@@ -34,6 +36,7 @@
     [super viewDidLoad];
     self.context = [DataManager sharedManager];
     [self.context managedObjectContext];
+    self.calendar = [NSCalendar currentCalendar];
     UIBarButtonItem *btnSetting = [[UIBarButtonItem alloc] init];
     [btnSetting setImage:[UIImage imageNamed:@"gear.png"]];
     [btnSetting setAction:@selector(btnSettings_Tab:)];
@@ -58,11 +61,14 @@
     for (int i = 0; i <= 3; i++) {
         UIButton *newButton = [[UIButton alloc] initWithFrame:_histogramView.frame];
         newButton.frame = CGRectMake(i * btnWidth, viewHeight - btnHeight, btnWidth, btnHeight);
+        [newButton setTag:i];
+        [newButton addTarget:self action:@selector(btnRequestFromButtonTag:) forControlEvents:UIControlEventTouchUpInside];
         [newButton setTitle:btnCaptions[i] forState:UIControlStateNormal];
         [newButton setTitleColor:RGB(0, 0, 0) forState:UIControlStateSelected];
         [newButton setTitleColor:RGB(44, 62, 80) forState:UIControlStateNormal];
         [_histogramView insertSubview:newButton atIndex:1];
     }
+    
 //    NSArray *values = @[@3200, @3300, @3400, @3255, @3600, @3200, @2900, @3100, @3000, @3200, @3350, @3150];
 ////    HistogramView *histogram = [[HistogramView alloc] initWithFrame:_histogramView.bounds];
 ////    histogram.width = [UIScreen mainScreen].bounds.size.width - 40;
@@ -85,18 +91,72 @@
 //    [_histogramView insertSubview:histogram atIndex:0];
 }
 
-- (IBAction)add:(id)sender {
-    NSManagedObject *newCal = [NSEntityDescription insertNewObjectForEntityForName:@"Eating" inManagedObjectContext:self.context.managedObjectContext];
+//- (IBAction)add:(id)sender {
+//    NSManagedObject *newCal = [NSEntityDescription insertNewObjectForEntityForName:@"Eating" inManagedObjectContext:self.context.managedObjectContext];
+//    
+//    NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
+//    f.numberStyle = NSNumberFormatterDecimalStyle;
+//    NSNumber *myNumber = [f numberFromString:_txt.text];
+//    
+//    [newCal setValue:myNumber forKey:@"calories"];
+//    [newCal setValue:[NSDate date] forKey:@"date"];
+//    [self.context saveContext];
+//    NSFetchRequest *req = [[NSFetchRequest alloc] initWithEntityName:@"Eating"];
+//    self.calories = [[self.context.managedObjectContext executeFetchRequest:req error:nil] mutableCopy];
+//    NSMutableArray *res = [[NSMutableArray alloc] init];
+//    for (int i = 0; i <= self.calories.count - 1; i++) {
+//        Eating *getCal = [self.calories objectAtIndex:i];
+//        [getCal valueForKey:@"calories"];
+//        [res addObject:getCal];
+//    }
+//    NSLog(@"%@", res);
+////    if (res.count > 0 ) {
+////    HistogramView *histogram = [[HistogramView alloc] initWithFrame:_histogramView.bounds
+////                                                     histogramWidth:[UIScreen mainScreen].bounds.size.width - 40
+////                                                             values:res
+////                                                           maxValue:5000
+////                                                      minTitleValue:1200
+////                                                           barDelta:0
+////                                                         firstColor:RGB(1, 225, 255)
+////                                                        secondColor:RGB(0, 209, 239)
+////                                                       withUpBorder:8
+////                                                      andLeftBorder:8];
+////    //    [histogram drawWithValues:values];
+////    [_histogramView insertSubview:histogram atIndex:0];
+////    }
+//}
+
+- (void)btnSettings_Tab:(UIBarButtonItem *)sender {
+    SettingsViewController *settingsVC = [[SettingsViewController alloc] init];
+    [self.navigationController pushViewController:settingsVC animated:YES];
+}
+
+- (void)btnRequestFromButtonTag:(UIButton *)button {
+    NSDateComponents *dateComponent = [[NSDateComponents alloc] init];
+    NSDate *newDate;
+    switch (button.tag) {
+        case 0:
+            dateComponent.day = 0;
+            break;
+        case 1:
+            dateComponent.weekOfYear = -1;
+            break;
+        case 2:
+            dateComponent.month = -1;
+            break;
+        case 3:
+            dateComponent.year = -1;
+            break;
+        default:
+            break;
+    }
+    NSLog(@"%@ %@", self.calories, newDate);
+    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"Eating"];
+    newDate = [self.calendar dateByAddingComponents:dateComponent toDate:[NSDate date] options:0];
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"date >= %@", newDate];
+    [request setPredicate:predicate];
+    self.calories = [[self.context.managedObjectContext executeFetchRequest:request error:nil] mutableCopy];
     
-    NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
-    f.numberStyle = NSNumberFormatterDecimalStyle;
-    NSNumber *myNumber = [f numberFromString:_txt.text];
-    
-    [newCal setValue:myNumber forKey:@"calories"];
-    [newCal setValue:[NSDate date] forKey:@"date"];
-    [self.context saveContext];
-    NSFetchRequest *req = [[NSFetchRequest alloc] initWithEntityName:@"Eating"];
-    self.calories = [[self.context.managedObjectContext executeFetchRequest:req error:nil] mutableCopy];
     NSMutableArray *res = [[NSMutableArray alloc] init];
     for (int i = 0; i <= self.calories.count - 1; i++) {
         Eating *getCal = [self.calories objectAtIndex:i];
@@ -104,25 +164,6 @@
         [res addObject:getCal];
     }
     NSLog(@"%@", res);
-//    if (res.count > 0 ) {
-//    HistogramView *histogram = [[HistogramView alloc] initWithFrame:_histogramView.bounds
-//                                                     histogramWidth:[UIScreen mainScreen].bounds.size.width - 40
-//                                                             values:res
-//                                                           maxValue:5000
-//                                                      minTitleValue:1200
-//                                                           barDelta:0
-//                                                         firstColor:RGB(1, 225, 255)
-//                                                        secondColor:RGB(0, 209, 239)
-//                                                       withUpBorder:8
-//                                                      andLeftBorder:8];
-//    //    [histogram drawWithValues:values];
-//    [_histogramView insertSubview:histogram atIndex:0];
-//    }
-}
-
-- (void)btnSettings_Tab:(UIBarButtonItem *)sender {
-    SettingsViewController *settingsVC = [[SettingsViewController alloc] init];
-    [self.navigationController pushViewController:settingsVC animated:YES];
 }
 
 @end
