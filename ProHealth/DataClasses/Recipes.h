fc3733c2ff45e089aa66787540b51c6a84eb0b82
Vladimir Psyukalov <psyukalov@gmail.com>
2015-11-30 12:49:17 +0300

    add class recipes

---
+++ ProHealth/DataClasses/Recipes.h
@@ -0,0 +1,13 @@
+//
+//  Recipes.h
+//  ProHealth
+//
+//  Created by Vladimir Psyukalov on 30.11.15.
+//  Copyright © 2015 Natalia Zubareva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Recipes : NSObject
+
+@end
---
+++ ProHealth/DataClasses/Recipes.m
@@ -0,0 +1,13 @@
+//
+//  Recipes.m
+//  ProHealth
+//
+//  Created by Vladimir Psyukalov on 30.11.15.
+//  Copyright © 2015 Natalia Zubareva. All rights reserved.
+//
+
+#import "Recipes.h"
+
+@implementation Recipes
+
+@end
