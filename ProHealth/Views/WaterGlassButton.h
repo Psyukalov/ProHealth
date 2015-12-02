756cc14f9c67f48809526c8ba170dd1e7532ed84
Vladimir Psyukalov <psyukalov@gmail.com>
2015-12-02 17:00:14 +0300

    added water glass button class

---
+++ ProHealth/Views/WaterGlassButton.h
@@ -0,0 +1,13 @@
+//
+//  WaterGlassButton.h
+//  ProHealth
+//
+//  Created by Vladimir Psyukalov on 02.12.15.
+//  Copyright © 2015 Natalia Zubareva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WaterGlassButton : UIButton
+
+@end
---
+++ ProHealth/Views/WaterGlassButton.m
@@ -0,0 +1,21 @@
+//
+//  WaterGlassButton.m
+//  ProHealth
+//
+//  Created by Vladimir Psyukalov on 02.12.15.
+//  Copyright © 2015 Natalia Zubareva. All rights reserved.
+//
+
+#import "WaterGlassButton.h"
+
+@implementation WaterGlassButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
