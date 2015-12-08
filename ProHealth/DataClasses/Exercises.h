ebd26de8bd1d72abc6789d1b13149fa2de1f2e92
Vladimir Psyukalov <psyukalov@gmail.com>
2015-12-08 16:24:59 +0300

    add exercises class

---
+++ ProHealth/DataClasses/Exercises.h
@@ -0,0 +1,13 @@
+//
+//  Exercises.h
+//  ProHealth
+//
+//  Created by Vladimir Psyukalov on 08.12.15.
+//  Copyright © 2015 Natalia Zubareva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Exercises : NSObject
+
+@end
---
+++ ProHealth/DataClasses/Exercises.m
@@ -0,0 +1,13 @@
+//
+//  Exercises.m
+//  ProHealth
+//
+//  Created by Vladimir Psyukalov on 08.12.15.
+//  Copyright © 2015 Natalia Zubareva. All rights reserved.
+//
+
+#import "Exercises.h"
+
+@implementation Exercises
+
+@end
