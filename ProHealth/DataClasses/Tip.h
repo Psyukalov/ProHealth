7f3a396b4f48b0b02e8c9e6d231cfe2849ef4357
Vladimir Psyukalov <psyukalov@gmail.com>
2015-12-09 11:50:28 +0300

    add tip class

---
+++ ProHealth/DataClasses/Tip.h
@@ -0,0 +1,13 @@
+//
+//  Tip.h
+//  ProHealth
+//
+//  Created by Vladimir Psyukalov on 09.12.15.
+//  Copyright © 2015 Natalia Zubareva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tip : NSObject
+
+@end
---
+++ ProHealth/DataClasses/Tip.m
@@ -0,0 +1,13 @@
+//
+//  Tip.m
+//  ProHealth
+//
+//  Created by Vladimir Psyukalov on 09.12.15.
+//  Copyright © 2015 Natalia Zubareva. All rights reserved.
+//
+
+#import "Tip.h"
+
+@implementation Tip
+
+@end
